4670c8f8a676771a54a42e7cd6e2ad7e114230e7
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2026-02-08 11:57:58 -0300

    Update concluir_tarefa.sh

--- ListadeTarefas/modules/concluir_tarefa.sh
+++ ListadeTarefas/modules/concluir_tarefa.sh
@@ -1,7 +1,7 @@
 concluir_tarefa() {
     listar_tarefas
-    read -p "Digite o número da tarefa concluída: " num
+    read -p "Digite o número da tarefa concluída: " id
 
-    sed -i "${num}s/^\[ \]/[X]/" "$TASK_FILE"
+    sed -i "${id}s/^\[ \]/[X]/" "$TASK_FILE"
     echo "Tarefa marcada como concluída!"
 }
