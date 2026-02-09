f65df0386408a284050592145d92e53575199ab4
Paulo Guilherme <pauloguilherme_cn@outlook.com>
2026-02-09 19:56:04 -0300

    Fix bugs

--- ListadeTarefas/modules/concluir_tarefa.sh
+++ ListadeTarefas/modules/concluir_tarefa.sh
@@ -15,43 +15,13 @@ concluir_tarefa() {
     fi
 
     # verifica se o ID existe
-    awk -F'|' -v id="$id" '
-        {
-            split($1, a, " ")
-            if (a[2] == id) exit 0
-        }
-        END { exit 1 }
-    ' "$TASK_FILE"
-
-    if [ $? -ne 0 ]; then
+    if ! grep -q "^\[[ X]\][[:space:]]\+$id[[:space:]]*\|" "$TASK_FILE"; then
         echo "Erro: tarefa não encontrada."
         return
     fi
-
-    # verifica se já está concluída
-    awk -F'|' -v id="$id" '
-        {
-            split($1, a, " ")
-            if (a[1] == "[X]" && a[2] == id) exit 0
-        }
-        END { exit 1 }
-    ' "$TASK_FILE"
-
-    if [ $? -eq 0 ]; then
-        echo "Essa tarefa já está concluída."
-        return
-    fi
-
-    # marca como concluída (somente essa tarefa)
-    awk -F'|' -v id="$id" '
-        {
-            split($1, a, " ")
-            if (a[2] == id && a[1] == "[ ]") {
-                $1 = "[X] " id
-            }
-            print $1 " | " $2 " | " $3
-        }
-    ' "$TASK_FILE" > "$TASK_FILE.tmp" && mv "$TASK_FILE.tmp" "$TASK_FILE"
-
+    
+    # apenas troca [ ] por [X] NA LINHA DO ID
+    sed -i "/^\[ \][[:space:]]*$id[[:space:]]*|/s/^\[ \]/[X]/" "$TASK_FILE"
     echo "Tarefa marcada como concluída!"
+
 }
