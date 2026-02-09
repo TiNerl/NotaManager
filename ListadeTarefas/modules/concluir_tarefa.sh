45228e581bc6f61cf79018eb0b347bb104c739c4
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2026-02-09 17:23:20 -0300

    Enhance concluir_tarefa function with validation
    Add checks for task existence and completion status.

--- ListadeTarefas/modules/concluir_tarefa.sh
+++ ListadeTarefas/modules/concluir_tarefa.sh
@@ -1,7 +1,28 @@
 concluir_tarefa() {
+
+    # verifica se o arquivo existe
+    if [ ! -f "$TASK_FILE" ]; then
+        echo "Arquivo de tarefas não encontrado."
+        return
+    fi
+
+    #Solicita ao usuário o número da tarefa que deverá ser marcada como concluída
     listar_tarefas
     read -rp "Digite o número da tarefa concluída: " id
 
+    # verifica se é número
+    if ! [[ "$id" =~ ^[0-9]+$ ]]; then
+        echo "Erro: digite um número válido."
+        return
+    fi
+
+    # verifica se a tarefa existe
+    if ! grep -q "| $id$" "$TASK_FILE"; then
+        echo "Erro: tarefa não encontrada."
+        return
+    fi
+
+    # marca como concluída
     sed -i "/| $id$/ s/^\[ \]/[X]/" "$TASK_FILE"
 
     if [[ -n "$(command -v dialog)" ]]; then
@@ -10,5 +31,11 @@ concluir_tarefa() {
         clear
     else
         echo "Tarefa marcada como concluída!"
+    fi
+
+    #Avisa se a tarefa já estiver concluída
+    if grep -q "^\[X\].*| $id$" "$TASK_FILE"; then
+    echo "Essa tarefa já está concluída."
+    return
     fi
 }
