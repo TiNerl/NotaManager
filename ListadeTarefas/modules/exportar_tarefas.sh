8d67d10d2204e3718aa8cb84d776bfdda59e9b1b
zehelll <aviz.h@escolar.ifrn.edu.br>
2026-02-09 21:18:01 -0300

    Update exportar_tarefas.sh

--- ListadeTarefas/modules/exportar_tarefas.sh
+++ ListadeTarefas/modules/exportar_tarefas.sh
@@ -1,24 +1,24 @@
 exportar_tarefas() {
 
-    # verifica se o arquivo de tarefas existe
+    #Verifica se o arquivo de tarefas existe
     if [ ! -f "$TASK_FILE" ]; then
         echo "Erro: arquivo de tarefas não encontrado."
         return
     fi
 
-    # solicita o nome do arquivo de exportação
+    #Solicita o nome do arquivo de exportação
     read -rp "Nome do arquivo para exportação: " arquivo
     
     #Coloca a extensão .txt no arquivo
     [[ "$arquivo" != *.txt ]] && arquivo="$arquivo.txt"
 
-    # verifica se o nome do arquivo foi informado
+    #Verifica se o nome do arquivo foi informado
     if [ -z "$arquivo" ]; then
         echo "Erro: nome do arquivo não pode ser vazio."
         return
     fi
 
-    # verifica se o arquivo de destino já existe
+    #Verifica se o arquivo de destino já existe
     if [ -e "$arquivo" ]; then
         read -rp "O arquivo já existe. Deseja sobrescrever? (s/n): " resp
         if [[ ! "$resp" =~ ^[sS]$ ]]; then
@@ -27,7 +27,7 @@ exportar_tarefas() {
         fi
     fi
 
-    # tenta copiar o arquivo e verifica se houve erro
+    #Tenta copiar o arquivo e verifica se houve erro
     if cp "$TASK_FILE" "$arquivo" 2>/dev/null; then
         if [[ -n "$(command -v dialog)" ]]; then
             clear
