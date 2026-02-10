59840f0cfec9fd8695cdaa87ce866de6be3d6a07
zehelll <aviz.h@escolar.ifrn.edu.br>
2026-02-09 21:12:49 -0300

    Update concluir_tarefa.sh

--- ListadeTarefas/modules/concluir_tarefa.sh
+++ ListadeTarefas/modules/concluir_tarefa.sh
@@ -1,28 +1,31 @@
 concluir_tarefa() {
 
+    #Verifica se existe o arquivo com as tarefas
     if [ ! -f "$TASK_FILE" ]; then
         echo "Arquivo de tarefas não encontrado."
         return
     fi
 
+    #Solicita ID ao usuário para marcar a tarefa concluída
     listar_tarefas
     read -rp "Digite o ID da tarefa concluída: " id
 
-    # valida número
+    #Verifica se o ID é apenas números (0 a 9) e tem pelo menos um dígito
     if ! [[ "$id" =~ ^[0-9]+$ ]]; then
         echo "Erro: digite um número válido."
         return
     fi
 
-    # verifica se o ID existe
+    # verifica se existe uma tarefa com o ID informado dentro do arquivo de tarefas.
     if ! grep -q "^\[[ X]\][[:space:]]\+$id[[:space:]]*\|" "$TASK_FILE"; then
         echo "Erro: tarefa não encontrada."
         return
     fi
     
-    # apenas troca [ ] por [X] NA LINHA DO ID
+    #Troca [ ] por [X] NA LINHA DO ID
     sed -i "/^\[ \][[:space:]]*$id[[:space:]]*|/s/^\[ \]/[X]/" "$TASK_FILE"
-    
+
+    #Mostra um textbox com dialog no caso de dar certo marcar a tarefa como concluída
     if [[ -n "$(command -v dialog)" ]]; then
         clear
         dialog --msgbox "Tarefa concluida com sucesso!" 6 50
