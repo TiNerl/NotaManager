540385657b396ae2759943563bb607a94ed9d485
zehelll <aviz.h@escolar.ifrn.edu.br>
2026-02-09 21:16:51 -0300

    Update excluir_tarefa.sh

--- ListadeTarefas/modules/excluir_tarefa.sh
+++ ListadeTarefas/modules/excluir_tarefa.sh
@@ -1,29 +1,31 @@
 excluir_tarefa() {
 
-  # verifica se o arquivo existe
+  #Verifica se o arquivo existe
   if [ ! -f "$TASK_FILE" ]; then
     echo "Arquivo de tarefas não encontrado."
     return
   fi
-
+  
+  #Solicita o ID da tarefa que deve ser excluído e mostra as tarefas adicionadas
   listar_tarefas
   read -rp "Digite o ID da tarefa que deseja excluir: " id
 
-  # valida se é número
+  #Verifica se o ID é apenas números (0 a 9) e tem pelo menos um dígito
   if ! [[ "$id" =~ ^[0-9]+$ ]]; then
     echo "Por favor, digite apenas números."
     return
   fi
 
-  # verifica se a tarefa existe (ID no começo)
+  #Verifica se existe uma tarefa com o ID informado dentro do arquivo de tarefas
   if ! grep -q "^\[[ X]\][[:space:]]*$id[[:space:]]*|" "$TASK_FILE"; then
     echo "Erro: tarefa não encontrada."
     return
   fi
 
-  # exclui SOMENTE a tarefa com esse ID
+  #Exclui SOMENTE a tarefa com esse ID
   sed -i "/^\[[ X]\][[:space:]]*$id[[:space:]]*|/d" "$TASK_FILE"
 
+  #Mostra um textbox com dialog no caso de dar certo excluir
   if [[ -n "$(command -v dialog)" ]]; then
     clear
     dialog --msgbox "Tarefa excluida com sucesso!" 6 50
